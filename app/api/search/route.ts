1a8a63b2b2e3915ab2ea3456accbe658cb1c8b7c
Rajiv Seelam <rajiv@betalectic.com>
2025-04-01 17:27:25 +0530

    Add search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,30 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { docsSource, blogSource } from "@/lib/source";
+import { createSearchAPI } from "fumadocs-core/search/server";
 
-export const { GET } = createFromSource(source);
+export const revalidate = false;
+
+export const { staticGET: GET } = createSearchAPI("advanced", {
+  indexes: [
+    ...docsSource.getPages().map((page) => {
+      return {
+        title: page.data.title,
+        description: page.data.description,
+        url: page.url,
+        id: page.url,
+        structuredData: page.data.structuredData,
+        tag: "docs",
+      };
+    }),
+    ...blogSource.getPages().map((page) => {
+      console.log("Blog Page URL:", page.url);
+      return {
+        title: page.data.title,
+        description: page.data.description,
+        url: page.url,
+        id: page.url,
+        structuredData: page.data.structuredData,
+        tag: "blog",
+      };
+    }),
+  ],
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,54 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import localFont from "next/font/local";
+import { baseUrl, createMetadata } from "@/components/metadata";
+import { description } from "./layout.config";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
+});
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export const metadata = createMetadata({
+  title: {
+    template: "%s | SaaS Foundations",
+    default: "SaaS Foundations",
+  },
+  description: description,
+  metadataBase: baseUrl,
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        <script
+          crossOrigin="anonymous"
+          src="//cdn.jsdelivr.net/npm/meta-scan@0.10.2/dist/auto.global.js"
+          data-auto-enable={"true"}
+        />
+      </head>
+      <body
+        className={cn(
+          "bg-background overscroll-none font-sans antialiased",
+          `${geistSans.variable} ${geistMono.variable}`
+        )}
+      >
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
