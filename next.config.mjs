6e1b2074c7d43977582989da137ac7ba604d24a6
Rajiv Seelam <rajiv@betalectic.com>
2025-04-01 15:16:42 +0530

    feat: add blogs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  serverExternalPackages: [
+    "ts-morph",
+    "typescript",
+    "oxc-transform",
+    "twoslash",
+  ],
 };
 
 export default withMDX(config);
--- source.config.ts
+++ source.config.ts
@@ -1,11 +1,60 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import {
+  defineCollections,
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
+import { transformerTwoslash } from "fumadocs-twoslash";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import { transformerRemoveNotationEscape } from "@shikijs/transformers";
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
+});
+
+export const blog = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z
+      .string()
+      .or(z.date())
+      .transform((value, context) => {
+        try {
+          return new Date(value);
+        } catch {
+          context.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Invalid date",
+          });
+          return z.NEVER;
+        }
+      }),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
+  }),
 });
 
 export default defineConfig({
+  lastModifiedTime: "git",
   mdxOptions: {
-    // MDX options
+    rehypeCodeOptions: {
+      inline: "tailing-curly-colon",
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash(),
+        transformerRemoveNotationEscape(),
+      ],
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
   },
 });
