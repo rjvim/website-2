0825adc6faf0fd75a0020cadddd495236e6c4e73
Rajiv Seelam <rajiv@betalectic.com>
2025-04-03 14:49:07 +0530

    Update env variable from VERCEL_PROJECT_PRODUCTION_URL to NEXT_PUBLIC_SITE_URL

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -28,7 +28,6 @@ export function createMetadata(override: Metadata): Metadata {
 }
 
 export const baseUrl =
-  process.env.NODE_ENV === "development" ||
-  !process.env.VERCEL_PROJECT_PRODUCTION_URL
+  process.env.NODE_ENV === "development" || !process.env.NEXT_PUBLIC_SITE_URL
     ? new URL("http://localhost:3001")
-    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
+    : new URL(`https://${process.env.NEXT_PUBLIC_SITE_URL}`);
--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 
 const baseUrl =
-  process.env.NODE_ENV === "development" ||
-  !process.env.VERCEL_PROJECT_PRODUCTION_URL
+  process.env.NODE_ENV === "development" || !process.env.NEXT_PUBLIC_SITE_URL
     ? new URL("https://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
+    : new URL(`https://${process.env.NEXT_PUBLIC_SITE_URL}`);
 
 module.exports = {
   siteUrl: baseUrl,
